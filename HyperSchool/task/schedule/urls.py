5514e3ab865f7f5dbb6047740391ba9a42e51edf
ibnabii <someone@somewhere.net>
2022-10-16 15:17:49 +0200

    Stage 4

--- HyperSchool/task/schedule/urls.py
+++ HyperSchool/task/schedule/urls.py
@@ -1,8 +1,19 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.views.generic import CreateView
+
 from . import views
+from . import models
 
 urlpatterns = [
     path('schedule/main/', views.main, name='main'),
     path('schedule/course_details/<int:pk>', views.CourseDetailView.as_view(), name='course-detail'),
     path('schedule/teacher_details/<int:pk>', views.TeacherDetailView.as_view(), name='teacher-detail'),
+    path('schedule/add_course/',
+        CreateView.as_view(
+            model=models.Student,
+            fields='__all__',
+            success_url=reverse_lazy('enroll'),
+            template_name='schedule/enroll.html'
+        ),
+        name='enroll'),
 ]
